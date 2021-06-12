bc6f4a64137ecec74c365e4f7685293f3339d612
Dong Jianhui <978007503@qq.com>
2021-06-12 19:05:49 +0800

    feat(rest): support rest protocol imports

---
+++ rest/nacos/rest_nacos.go
@@ -0,0 +1,9 @@
+package nacos
+
+import (
+	_ "github.com/dubbogo/imports/rest/nacos/consul"
+	_ "github.com/dubbogo/imports/rest/nacos/etcd"
+	_ "github.com/dubbogo/imports/rest/nacos/kubernetes"
+	_ "github.com/dubbogo/imports/rest/nacos/nacos"
+	_ "github.com/dubbogo/imports/rest/nacos/zk"
+)
