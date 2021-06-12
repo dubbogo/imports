c7801ec4e319eada94079ae2697824692fea7688
Dong Jianhui <978007503@qq.com>
2021-06-12 18:45:47 +0800

    feat(dubbo3): support dubbo3 protocol imports

---
+++ dubbo3/file/dubbo3_file.go
@@ -0,0 +1,9 @@
+package nacos
+
+import (
+	_ "github.com/dubbogo/imports/dubbo3/file/consul"
+	_ "github.com/dubbogo/imports/dubbo3/file/etcd"
+	_ "github.com/dubbogo/imports/dubbo3/file/kubernetes"
+	_ "github.com/dubbogo/imports/dubbo3/file/nacos"
+	_ "github.com/dubbogo/imports/dubbo3/file/zk"
+)
---
+++ dubbo3/zk/dubbo3_zk.go
@@ -0,0 +1,9 @@
+package zk
+
+import (
+	_ "github.com/dubbogo/imports/dubbo3/zk/consul"
+	_ "github.com/dubbogo/imports/dubbo3/zk/etcd"
+	_ "github.com/dubbogo/imports/dubbo3/zk/kubernetes"
+	_ "github.com/dubbogo/imports/dubbo3/zk/nacos"
+	_ "github.com/dubbogo/imports/dubbo3/zk/zk"
+)
