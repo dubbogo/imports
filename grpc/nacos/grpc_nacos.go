093eda4318ebe959b23d824199c95fdafa900fe0
Dong Jianhui <978007503@qq.com>
2021-06-12 18:51:49 +0800

    feat(grpc): support grpc protocol imports

---
+++ grpc/nacos/grpc_nacos.go
@@ -0,0 +1,9 @@
+package nacos
+
+import (
+	_ "github.com/dubbogo/imports/grpc/nacos/consul"
+	_ "github.com/dubbogo/imports/grpc/nacos/etcd"
+	_ "github.com/dubbogo/imports/grpc/nacos/kubernetes"
+	_ "github.com/dubbogo/imports/grpc/nacos/nacos"
+	_ "github.com/dubbogo/imports/grpc/nacos/zk"
+)
---
+++ grpc/zk/grpc_zk.go
@@ -0,0 +1,9 @@
+package zk
+
+import (
+	_ "github.com/dubbogo/imports/grpc/zk/consul"
+	_ "github.com/dubbogo/imports/grpc/zk/etcd"
+	_ "github.com/dubbogo/imports/grpc/zk/kubernetes"
+	_ "github.com/dubbogo/imports/grpc/zk/nacos"
+	_ "github.com/dubbogo/imports/grpc/zk/zk"
+)
